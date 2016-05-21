96e6e07cddb281646e171948d8e3f1c285eadb4d
BENMESSAOUD Mehdi <dm_benmessaoud@esi.dz>
2016-05-21 13:10:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
 	'secret': '3macs',
-	'database': '  mongodb://3macs:ihealth@ds011943.mlab.com:11943/ihealth'
+	'database': 'mongodb://3macs:ihealth@ds011943.mlab.com:11943/ihealth'
 
 };
